506ceccf06fc1816499631ef8c7207dc02346bcf
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-26 22:24:12 +0800

    Delete "text-gray-900"

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-w-xs text-gray-900 antialiased">
+      <body className="min-w-xs antialiased">
         <Main />
         <NextScript />
       </body>
