c90068f7a6832377e8be8160bb03212440827134
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-01-13 13:19:53 +0800

    Create header nav

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,44 @@
 import Title from '@components/Title'
-import Header from '@components/Header'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-azure text-dark-400">
+    <div>
       <Title>Family Fortunate</Title>
-      <Header />
+
+      <header>
+        <div className="container absolute inset-x-0 top-0 mx-auto flex w-full items-center justify-between p-6">
+          <div>
+            <a className="font-display text-2xl" href="">
+              Family Fortunate
+            </a>
+          </div>
+
+          <nav className="hidden lg:flex lg:items-center lg:gap-6 lg:text-sm xl:gap-8">
+            <a className="font-semibold text-malachite-600" href="">
+              Home
+            </a>
+            <a href="">How it works</a>
+            <a href="">Inspiration</a>
+            <a href="">Contact</a>
+            <a href="">FAQ</a>
+          </nav>
+
+          <button className="lg:hidden" type="button">
+            <Bars3Icon className="h-6 w-6" />
+          </button>
+
+          <div className="hidden lg:flex lg:items-center lg:gap-4 lg:text-sm">
+            <a className="font-semibold text-malachite-600" href="">
+              Log in
+            </a>
+
+            <a className="rounded-lg bg-malachite-600 px-4 py-3 font-medium text-white" href="">
+              Create an account
+            </a>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
