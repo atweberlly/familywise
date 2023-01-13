ace3b96a06fb273ff5f487c0480501f1b859509b
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-01-13 13:36:09 +0800

    Delete configured font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/components/**/*.{js,jsx}', './src/pages/**/*.{js,jsx}'],
   theme: {
-    // Modified font sizes and their line heights
-    fontSize: {
-      '6xl': ['6rem', { lineHeight: 1.25 }],
-      '5xl': ['4rem', { lineHeight: 1.25 }],
-      '4xl': ['3rem', { lineHeight: 1.25 }],
-      '3xl': ['2.5rem', { lineHeight: 1.25 }],
-      '2xl': ['1.5rem', { lineHeight: 1.25 }],
-      xl: ['1.125rem', { lineHeight: '1.75rem' }],
-      base: ['1rem', { lineHeight: '1.5rem' }],
-      sm: ['0.875rem', { lineHeight: '1.25rem' }],
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-    },
     extend: {
       colors: {
         malachite: {
