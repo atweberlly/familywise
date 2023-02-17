31bfd5f50a3c1afb64a56e05805bc6f1c4f20574
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-02-17 16:16:36 +0800

    Create "Preserving precious memories"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -168,7 +168,51 @@ export default function Home() {
               alt="Get Started"
               width="312"
               height="416"
+              priority="false"
             />
+          </div>
+        </div>
+      </div>
+
+      <div className="px-10 pt-14 pb-6">
+        <div className="grid grid-cols-1 gap-8">
+          <div>
+            <h2 className="font-serif text-6xl font-bold">
+              <span className="block text-[#ca8e22]/30">Preserving</span>{' '}
+              <span className="ml-16 block italic text-[#ca8e22]">precious memories.</span>
+            </h2>
+
+            <p className="mt-8">
+              As a funeral celebrant of 9 years, our founder Rachel Michael has heard many stories
+              from families after their loved ones have passed away. Over time, she&apos;s realized
+              there are many more stories that families never get to hear.
+            </p>
+
+            <p className="mt-4">
+              Family Fortunate was inspired by her work and is designed to give families the
+              opportunity to gather precious memories whilst the opportunity is there.
+            </p>
+
+            <p className="mt-4">
+              Family Fortunate is dedicated to the thousands of people she has had the privilege to
+              assist as they grieve the loss of the people they love.
+            </p>
+          </div>
+
+          <div>
+            <Image
+              className="h-[456px] w-full object-cover object-center"
+              src="/images/founder/rachel-michael-portrait@4x.jpg"
+              alt="Rachel Michael"
+              width="312"
+              height="456"
+              priority="false"
+            />
+
+            <div className="mt-5">
+              <div className="font-serif text-[2rem] font-bold leading-none">Rachel Michael</div>
+              <div className="mt-1 text-lg font-medium">Founder</div>
+            </div>
           </div>
         </div>
       </div>
