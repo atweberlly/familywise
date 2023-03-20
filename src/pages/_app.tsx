aa130d019960a6c28d739d21f88425d52eef72d5
jonahcastro <jonahmay.castro08@gmail.com>
2023-03-20 10:03:34 +0800

    Flatpickr css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { store } from '../app/store'
 import '../styles/main.css'
+import 'flatpickr/dist/themes/light.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
