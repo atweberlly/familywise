2211f18ddcc0a7c0b291905516f3abfb7fb5de1d
jonahcastro <jonahmay.castro08@gmail.com>
2023-03-27 03:42:22 +0800

    Testing: email not working

--- src/pages/api/questions/getFirstQuestion.js
+++ src/pages/api/questions/getFirstQuestion.js
@@ -8,48 +8,46 @@ const staticQuestionId = '641e466788e2fe3884a27b0f' //requested by the client
 
 const getFirstQuestion = async (req, res) => {
   try {
+    // Connect to the database
     await dbConnect()
-    // Extract relevant information from request body
-    const { _id } = req.body
+    // Extract the user data from the request body
+    const user = req.body
+    // Construct the email subject line
+    const subject = `${capitalizeFirstLetter(user.firstname)}, Here's your first question!`
+    // Set the email template to be used
+    const template = 'Both/onboarding-2.html'
 
     // Find the relevant question for the user
     const questions = await Questions.findOne({
       _id: staticQuestionId,
     })
     // Check if the user has already have their first question
-    const currentStories = await Story.find({ user_id: _id })
+    const currentStories = await Story.find({ user_id: user._id })
     if (currentStories.length !== 0) {
+      // If the user has already received their first question, send an error response
       return res.status(500).json({ message: 'First question already added' })
     }
+    // Set the parameters for the email template
+    const params = {
+      name: capitalizeFirstLetter(user.firstname),
+      question: questions.question,
+    }
+    // Set the configuration for the email to be sent
+    const emailConfig = {
+      subject: subject,
+      template: template,
+      param: params,
+      to: user.email,
+    }
     // Send the onboarding email and first question
-    await sendFirstQuestion(req.body, questions.question)
+    await sendMailFnx(emailConfig)
     // Save the user's first question to the story table
-    await Story.create({ user_id: _id, question_id: questions._id })
-
+    await Story.create({ user_id: user._id, question_id: questions._id })
+    // Send a success response to the client
     return res.status(200).json({ message: 'Email successfully sent!' })
   } catch (err) {
+    // If an error occurs, send an error response to the client
     return res.status(500).json({ message: err })
   }
 }
-
-const sendFirstQuestion = async (user, question) => {
-  //params { subject, template, param, to }
-  const subject = `${capitalizeFirstLetter(user.firstname)}, Here's your first question!`
-  const params = {
-    name: capitalizeFirstLetter(user.firstname),
-    question: question,
-  }
-
-  const template = 'Both/onboarding-2.html'
-
-  const emailConfig = {
-    subject: subject,
-    template: template,
-    param: params,
-    to: user.email,
-  }
-  // Send the email
-  await sendMailFnx(emailConfig)
-}
-
 export default getFirstQuestion
