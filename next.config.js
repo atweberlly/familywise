d373733313cbfd285337b21ae06a786206963d89
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-08-25 15:23:36 +0800

    Disable lint issues on build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   pageExtensions: ['ts', 'tsx'],
   reactStrictMode: true,
   webpack(config) {
