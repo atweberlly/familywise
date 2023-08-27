2a2698d88267d9012457900ee6386afb480f9a79
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-08-27 13:20:45 +0800

    Create new-landing-page.tsx

---
+++ src/pages/new-landing-page.tsx
@@ -0,0 +1,13 @@
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+import Title from '~/components/Title'
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Title>FamilyWise</Title>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
