bc8a073c2da2acad81e55a313025bda1daae35cc
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-08-27 13:11:45 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,82 +1,125 @@
+import Image from 'next/image'
 import Link from 'next/link'
-import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Facebook from '~/public/svg/facebook.svg'
+import Instagram from '~/public/svg/instagram.svg'
+import Pinterest from '~/public/svg/pinterest.svg'
+import YouTube from '~/public/svg/youtube.svg'
 
-export default function Footer() {
+interface Hyperlink {
+  id: number
+  href: string
+  text: string
+}
+
+const information: Hyperlink[] = [
+  { id: 1, href: '', text: 'About' },
+  { id: 2, href: '', text: 'Membership Options' },
+  { id: 3, href: '', text: 'News and Blogs' },
+  { id: 4, href: '', text: 'FAQs' },
+]
+
+const legal: Hyperlink[] = [
+  { id: 1, href: '', text: 'Terms and Conditions' },
+  { id: 2, href: '', text: 'Privacy Options' },
+  { id: 3, href: '', text: 'Cookies' },
+  { id: 4, href: '', text: 'Refund' },
+]
+
+export default function Footer(): JSX.Element {
   return (
-    <footer className="bg-black-pearl pb-8 pt-12 text-white">
-      <div className="container grid grid-cols-1 gap-8 lg:grid-cols-3">
-        <div>
-          <img
-            className="w-38 inline-block"
-            src="/svg/family-fortunate-logotype-white.svg"
-            alt="Family Wise"
-            loading="lazy"
-          />
+    <footer className="bg-[#001822] py-8 text-primary-100">
+      <div className="container">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div className="md:col-span-2">
+            <Image
+              className="inline-block h-20 w-40 object-contain"
+              src="/images/familywise-logotype-white.png"
+              alt="FamilyWise Stories"
+              width={250}
+              height={112}
+            />
+            <p className="mt-1">Capturing Life&apos;s Priceless Moments</p>
+
+            <ul className="mt-5 space-y-5">
+              <li>
+                <div className="font-semibold text-primary-600">Address</div>
+                <address className="mt-1 not-italic">
+                  61-63 Parry St Newcastle, NSW Australia
+                </address>
+              </li>
+
+              <li>
+                <div className="font-semibold text-primary-600">E-mail us</div>
+                <a className="mt-1" href="mailto:hello@familywise.us">
+                  hello@familywise.us
+                </a>
+              </li>
+
+              <li>
+                <div className="font-semibold text-primary-600">Follow us on</div>
+                <div className="mt-2 inline-flex items-center space-x-2.5">
+                  <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <span className="sr-only">Facebook</span>
+                    <Facebook className="h-6 w-6" />
+                  </a>
+
+                  <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <span className="sr-only">Instagram</span>
+                    <Instagram className="h-6 w-6" />
+                  </a>
+
+                  <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                    <span className="sr-only">YouTube</span>
+                    <YouTube className="h-6 w-6" />
+                  </a>
+
+                  <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
+                    <span className="sr-only">Pinterest</span>
+                    <Pinterest className="h-6 w-6" />
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <div className="flex items-center">
+              <div className="h-px w-10 bg-current" />
+              <h2 className="ml-3 text-lg font-medium uppercase">Information</h2>
+            </div>
+
+            <ul className="mt-5 space-y-5">
+              {information.map((link) => (
+                <li key={link.id}>
+                  <Link href={link.href} aria-label={link.text}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <div className="flex items-center">
+              <div className="h-px w-10 bg-current" />
+              <h2 className="ml-3 text-lg font-medium uppercase">Legal</h2>
+            </div>
+
+            <ul className="mt-5 space-y-5">
+              {legal.map((link) => (
+                <li key={link.id}>
+                  <Link href={link.href} aria-label={link.text}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
-        <div className="mt-24 lg:mt-0">
-          <div className="text-sm font-medium uppercase tracking-wide text-lemon-curry">
-            Information
-          </div>
-          <ul className="mt-4 space-y-2 text-primary-100">
-            <li>
-              <Link href={'/pricing'}>Membership Options</Link>
-            </li>
-            <li>
-              <Link href={'/frequently-asked-questions'}>FAQs</Link>
-            </li>
-            <li>
-              <Link href={'/#testimonials'}>Testimonials</Link>
-            </li>
-            <li>
-              <a href={'/privacy'}>Privacy</a>
-            </li>
-            <li>
-              <a href={'/cookies'}>Cookie</a>
-            </li>
-            <li>
-              <a href={'/refund'}>Refund</a>
-            </li>
-            <li>
-              <a href={'/terms-and-condition'}>Terms & Conditions</a>
-            </li>
-          </ul>
-        </div>
+        <hr className="mb-4 mt-16 border-[#98a2b3]" />
 
-        <div>
-          <div className="text-sm font-medium uppercase tracking-wide text-lemon-curry">
-            Address
-          </div>
-          <div className="mt-4 text-primary-100">61-63 Parry St Newcastle, NSW Australia</div>
-
-          <div className="mt-8 text-sm font-medium uppercase tracking-wide text-lemon-curry">
-            Email us
-          </div>
-          <a className="mt-4 inline-block text-primary-100" href="mailto:mystory@familywise.us">
-            mystory@familywise.us
-          </a>
-          <div className="tracking-widetext-lemon-curry mt-4 text-sm font-medium uppercase">
-            Follow us
-          </div>
-          <div className="mt-4 flex items-center space-x-2.5">
-            <a href="https://www.facebook.com/familywise.stories" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faFacebookF} className="h-6 w-6" />
-            </a>
-
-            <a href="https://www.instagram.com/familywise.stories" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="container mt-16">
-        <hr className="mb-6 border-gray-600" />
-
-        <p className="mt-6 text-sm text-gray-500">
-          Copyright {new Date().getFullYear()} Family Wise. All rights reserved.
-        </p>
+        <p className="text-sm">Copyright 2023 FamilyWise Stories. All right reserved.</p>
       </div>
     </footer>
   )
