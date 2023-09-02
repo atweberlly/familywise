6bce4bda7e32263b33f96aa0cefb05ab14a6a095
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-09-02 16:53:53 +0800

    Import Playfair Display font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+        />
+      </Head>
       <body className="min-w-xs scroll-smooth antialiased">
         <Main />
         <NextScript />
