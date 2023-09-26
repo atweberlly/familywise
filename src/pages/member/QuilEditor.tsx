16a481a86e5eb1032125bf16e7c26ad02a5529d7
NeilJames <casteloneiljames1@gmail.com>
2023-09-26 16:56:31 +0800

    STT - 865d4nn0h

--- src/pages/member/QuilEditor.tsx
+++ src/pages/member/QuilEditor.tsx
@@ -2,6 +2,11 @@ import { useEffect, useRef, useState } from 'react'
 import toast from 'react-hot-toast'
 import 'react-quill/dist/quill.snow.css'
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { RootState } from '../../app/store'
+import { setUser } from '../../slices/slice'
+import axios from 'axios'
 
 // Import Quill styles
 
@@ -116,22 +121,73 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange, editorLoadin
     }
   }
 
+  const dispatch = useAppDispatch()
+  const user = useAppSelector((state: RootState) => state.userSlice.user)
+  const [isSpeakToTextAvailable, setIsSpeakToTextAvailable] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const respo = await axios.get('/api/users/getUser')
+        const userData = respo.data.user[0]
+
+        const planTypes = ['Free-Trial', 'Your-life-in-a-book', 'Premium']
+        if (planTypes.includes(userData.planType)) {
+          dispatch(setUser(userData))
+          setIsSpeakToTextAvailable(true)
+        } else {
+          setIsSpeakToTextAvailable(false)
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error)
+      }
+    }
+    fetchData()
+  }, [dispatch])
+
   return (
     <div>
       <span className="font-normal dark:text-gray-600">Your story</span>
       <div className="h-100 mt-[12px] min-h-[5vh] w-full rounded-[12px] border-[1.5px] border-secondary-500 px-[29px] py-[22px] text-[14px] text-secondary-600 focus:border-none dark:border-dark dark:bg-white dark:text-white">
-        <button
-          onClick={startRecording}
-          className={`h-100 text-3l mt-[12px] min-h-[5vh] w-full rounded-[12px] border-[1.5px]  px-[29px] py-[22px] text-white focus:border-none dark:border-white dark:bg-dark 
+        {/*<button onClick={startRecording}
+            className={`h-100 text-3l mt-[12px] min-h-[5vh] w-full rounded-[12px] border-[1.5px]  px-[29px] py-[22px] text-white focus:border-none dark:border-white dark:bg-dark 
+            dark:text-white
+            ${
+              recording
+                ? 'bg-secondary-500 hover:border-red-400 hover:text-red-400'
+                : 'bg-secondary-500 hover:border-red-400 hover:text-blue-400'
+              }`}
+          >
+            {recording ? `Recording (${duration}s)` : 'Speak'}
+            </button>*/}
+        {isSpeakToTextAvailable ? (
+          <button
+            onClick={startRecording}
+            className={`h-100 text-3l mt-[12px] min-h-[5vh] w-full rounded-[12px] border-[1.5px]  px-[29px] py-[22px] text-white focus:border-none dark:border-white dark:bg-dark 
           dark:text-white
           ${
             recording
               ? 'bg-secondary-500 hover:border-red-400 hover:text-red-400'
               : 'bg-secondary-500 hover:border-red-400 hover:text-blue-400'
           }`}
-        >
-          {recording ? `Recording (${duration}s)` : 'Speak'}
-        </button>
+          >
+            {recording ? `Recording (${duration}s)` : 'Speak'}
+          </button>
+        ) : (
+          <div className="disabled-button">
+            <Link href="checkout/[id].tsx">
+              <button
+                className="h-100 text-3l mt-[12px] min-h-[5vh] w-full rounded-[12px] border-[1.5px]  bg-secondary-500 px-[29px] py-[22px] text-white hover:border-red-200 hover:text-white-400 
+            focus:border-none dark:border-white dark:bg-dark dark:text-white"
+              >
+                Unlock Speech-To-Text Feature{' '}
+                <span className="mr-2 rounded border border-green-400 bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-500 dark:bg-gray-700 dark:text-green-400">
+                  Buy now
+                </span>
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
       {/* Display the transcript */}
       {recording && transcript.length > 0 && (
