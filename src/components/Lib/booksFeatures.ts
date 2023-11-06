5a963622114034646122b2783c10cf44f16aafa9
Jonah May <jonahmay.castro08@gmail.com>
2023-11-06 20:05:07 +0800

    Add book page

---
+++ src/components/Lib/booksFeatures.ts
@@ -0,0 +1,50 @@
+export interface Props {
+  id: number
+  heading: string
+  body: string
+}
+export const booksFeatures1: Array<Props> = [
+  {
+    id: 1,
+    heading: 'Question-Based book',
+    body: `<p>The essence of FamilyWise is your stories. With over 500 questions, prompts, and advice to guide you along the way, you&apos;ll never lack inspiration. </p>
+    <p>
+    You&apos;ll be surprised how quickly your legacy book takes shape.
+    </p>`,
+  },
+  {
+    id: 2,
+    heading: 'Photo-Based book',
+    body: `<p>Instead of starting with a question, choose a photo & write your memory of that moment in time or choose a drawing or artwork & write about what inspired it.</p>
+    <p>Perfect for children&apos;s birthday books, a holiday diary, a wedding book, recipe books, yearbooks, or for publishing your children&apos;s drawings & art. This works perfectly for any image-driven theme. You&apos;ll receive weekly emails with tips and prompts to guide you through the process.
+    </p>`,
+  },
+  {
+    id: 3,
+    heading: 'Blank book',
+    body: `<p>If you have your own content, we give you the framework to turn it into a book. It&apos;s the easiest way to become published with your own original work. </p>
+    <p>Perfect for a book of your poetry, family recipes, short stories, song lyrics or your favourite quotes.  Also great for affirmations, prayers, diary entries or any of your creative writing.</p>`,
+  },
+  {
+    id: 4,
+    heading: 'Your Memoirs',
+    body: `<p>With carefully selected questions designed to spark your memories, FamilyWise Stories leads you through your life&apos;s meaningful moments & treasured memories and guides you as you author your own keepsake book. </p>
+    <p>We send an email reminder each week so you could think of it as a guided memoir.</p>`,
+  },
+]
+
+export const booksFeatures2: Array<Props> = [
+  {
+    id: 1,
+    heading: 'Write as quickly as you want',
+    body: `<p>You don't have to be a professional writer to publish a book. We guide you with questions, tips and inspiration so your legacy book truly captures your life.  <p>
+    Once you start writing you&apos;ll see how easy it is to write a paragraph or a few pages.</p>`,
+  },
+  {
+    id: 2,
+    heading: 'Pure inspiration',
+    body: `<p>Our questions are designed to inspire you and remind you of your memories. It&apos;s not just your past, though; our prompts will get you thinking about your current life and your future too.</p>
+    <p>Our extensive bank of questions is clustered into collections of categories such as Who I Am Today, Starting Out, Treasured Possessions, Strongest Memories, and The Passage of Time.
+    </p>`,
+  },
+]
---
+++ src/pages/books.tsx
@@ -0,0 +1,138 @@
+import { useEffect } from 'react'
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline'
+import CoverCarousel from '~/components/CoverCarousel'
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+import { booksFeatures1, booksFeatures2 } from '~/components/Lib/booksFeatures'
+import Newsletter from '~/components/Newsletter'
+import PricingSection from '~/components/PricingThumbnail'
+import Testimonials from '~/components/Testimonials'
+import Title from '~/components/Title'
+
+export default function Books(): JSX.Element {
+  useEffect(() => {
+    function marquee(selector: string, speed: number) {
+      const parentSelector = document.querySelector(selector) as HTMLElement
+      const clone = parentSelector.innerHTML
+      const firstElement = parentSelector.children[0] as HTMLElement
+      let i = 0
+
+      parentSelector.insertAdjacentHTML('beforeend', clone)
+      parentSelector.insertAdjacentHTML('beforeend', clone)
+
+      setInterval(() => {
+        firstElement.style.marginLeft = `-${i}px`
+        if (i > firstElement.clientWidth) {
+          i = 0
+        }
+        i += speed
+      }, 0)
+    }
+
+    marquee('.how-it-works', 0.25)
+  }, [])
+
+  return (
+    <div>
+      <Title>Books</Title>
+      <header className="bg-white text-black">
+        <Header />
+      </header>
+
+      <div className="py-16 lg:py-24">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <div className="mx-auto max-w-3xl text-center">
+            <h2 className="font-serif text-4xl font-medium text-gray-900 lg:text-5xl">
+              Bring your memories to life with FamilyWise Stories
+            </h2>
+            <p className="mt-4 text-lg text-gray-600 lg:mt-6 lg:text-xl">
+              Compile your memories into your very own book of memories
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-[18rem] bg-gray-300 lg:h-[35rem]">{/* Image */}</div>
+
+      <div className="overflow-x-hidden py-16 lg:py-24">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <div className="how-it-works hidden lg:flex lg:items-center lg:space-x-8 lg:whitespace-nowrap lg:text-[#112f45]">
+            <div className="flex select-none items-center space-x-8 text-center font-serif text-8xl font-bold">
+              <span>Your memories</span>
+              <div className="mt-8 h-0.5 w-24 bg-black"></div>
+              <span>Your story</span>
+              <div className="mt-8 h-0.5 w-24 bg-black"></div>
+              <span>Your legacy</span>
+              <div className="mt-8 h-0.5 w-24 bg-black"></div>
+            </div>
+          </div>
+
+          <div className="mx-8 mr-4 text-center font-serif text-[3.25rem] font-bold leading-none lg:hidden">
+            Your stories, your memories
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto max-w-7xl px-4 md:px-8">
+        <div className="grid gap-8 md:grid-cols-2 lg:gap-16">
+          {booksFeatures1.map((feature: { id: number; heading: string; body: string }) => (
+            <div key={feature.id}>
+              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full border-8 border-[#fffcf8] bg-[#f9ecd4]">
+                <ChatBubbleLeftRightIcon className="h-6 w-6 text-[#ca8e22]" />
+              </div>
+              <div className="mt-6">
+                <h3 className="font-serif text-4xl">{feature.heading}</h3>
+                <div
+                  className="mt-4 space-y-4 text-gray-600"
+                  dangerouslySetInnerHTML={{ __html: feature.body }}
+                ></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <CoverCarousel />
+
+      <div className="mx-auto max-w-7xl px-4 py-16 md:px-8 lg:py-24">
+        <div className="grid gap-8 md:grid-cols-2 lg:gap-16">
+          {booksFeatures2.map((feature: { id: number; heading: string; body: string }) => (
+            <div key={feature.id}>
+              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full border-8 border-[#fffcf8] bg-[#f9ecd4]">
+                <ChatBubbleLeftRightIcon className="h-6 w-6 text-[#ca8e22]" />
+              </div>
+              <div className="mt-6">
+                <h3 className="font-serif text-4xl">{feature.heading}</h3>
+                <div
+                  className="mt-4 space-y-4 text-gray-600"
+                  dangerouslySetInnerHTML={{ __html: feature.body }}
+                ></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="py-16 lg:py-24">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <div className="mx-auto max-w-3xl text-center">
+            <h2 className="mt-3 font-serif text-4xl font-medium text-gray-900 lg:text-5xl">
+              What&apos;s a family tree without the stories?
+            </h2>
+            <p className="mt-4 text-lg text-gray-600 lg:mt-6 lg:text-xl">
+              If you have a family tree, you have the names of your ancestors, but do you know the
+              stories behind those names? FamilyWise Stories ensures your future generations will
+              remember you and your achievements.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-[18rem] bg-gray-300 lg:h-[35rem]">{/* Image */}</div>
+      <Testimonials />
+      <PricingSection />
+      <Newsletter />
+      <Footer />
+    </div>
+  )
+}
