066c6209f2801ef064521aebfc77a16f35e1f83f
Echo <140991513+jerichoweberlly@users.noreply.github.com>
2023-12-14 22:28:17 +0800

    update/added-success-gift/fix-text

--- src/pages/success_gift.tsx
+++ src/pages/success_gift.tsx
@@ -28,17 +28,24 @@ export default function Success(): JSX.Element {
 
         <div className="mx-auto my-5 max-w-4xl px-4 text-center md:px-8 ">
           <h3 className="text-xl font-semibold text-gray-900">
-            You&apos;ll receive an email soon so you can start right away.
+            You&apos;ll receive an email soon with more information about the beautiful gift
+            you&apos;ve purchased.
           </h3>
           <p className="mt-4 text-gray-600 lg:mt-6">
-            With carefully selected questions designed to spark your memories, FamilyWise Stories
-            will lead you through your life&apos;s meaningful moments & treasured memories and will
-            guide you as you author your own keepsake book.
+            With carefully selected questions designed to spark memories, FamilyWise Stories leads
+            our members through life’s meaningful moments & treasured memories and guides them as
+            they author their very own book.
           </p>
           <div className="my-4">
             <p className="mt-4 text-gray-600">
-              I hope you enjoy your walk down memory lane as you create a keepsake your whole family
-              will enjoy once your very own life story is published!
+              I hope they enjoy both the gift you&apos;ve chosen for them and their walk down memory
+              lane as they progress through this wonderful project.
+            </p>
+          </div>
+          <div className="my-4">
+            <p className="mt-4 text-gray-600">
+              Soon, there will be a treasured book for you and so many others to share and enjoy
+              together!
             </p>
           </div>
           <div className="pt-4">
